50c7ecf037b270d902372d68b4426723b8457ffb
Thomas Del Vecchio <thomasdelvecchio4@gmail.com>
2021-01-12 15:16:28 -0500

    Initial commit.

---
+++ setup2.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+cd /autograder/source
+source ./config.sh
+
+mkdir -p /root/.ssh
+cp ssh_config /root/.ssh/config
+# Make sure to include your private key here
+cp deploy_key /root/.ssh/deploy_key
+chmod 0600 /root/.ssh/deploy_key
+# To prevent host key verification errors at runtime
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+
+# Initialize git repo with assignment files
+git init
+git remote add origin git@github.com:tnelson/lfs.git
+git fetch --all
+git reset --hard origin/master 
+
+
+cd /autograder
+
+# Install racket
+apt update
+apt-get update -y
+apt install software-properties-common -y
+add-apt-repository ppa:plt/racket -y
+apt-get install -y racket
+apt-get clean
+
+# # Install forge
+# raco pkg install forge
+
+git clone https://github.com/tnelson/forge.git
+cd Forge/forge
+git checkout no-state
+raco pkg install
+
