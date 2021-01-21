cd428646dccd8bef57ded22591c112d562d49a62
Thomas Del Vecchio <thomasdelvecchio4@gmail.com>
2021-01-20 21:47:27 -0500

    Ignore output from code file, just take last line.

--- run_autograder.py
+++ run_autograder.py
@@ -21,7 +21,7 @@ def run_pair(code_file, test_file):
     os.system("racket temp/test.rkt > temp/result")
 
     with open("temp/result", "r") as f:
-        result = f.read()
+        result = f.read().split("\n")[-1]
 
     return result
 
