ddd1a629aab895468e8b2ed6a3c857eb03088db5
Thomas Del Vecchio <thomasdelvecchio4@gmail.com>
2021-02-04 09:42:07 -0500

    Changed ot use python so we don't need ts.

---
+++ is_before_late_deadline.py
@@ -0,0 +1,34 @@
+import sys
+import json
+import time
+import datetime
+
+def parse_command_line():
+    if len(sys.argv) != 2:
+        raise Exception("Usage: <meta_data_file>")
+
+    return sys.argv[1]
+
+def load_file(path):
+    with open(path, "r") as f:
+        return json.read(f)
+
+def main():
+    meta_data_file = parse_command_line()
+    data = load_file(meta_data_file)
+
+    if "late_due_date" in data["assignment"]:
+        due_date = data["assignment"]["late_due_date"]
+    else:
+        due_date = data["assignment"]["due_date"]
+
+    unix_due_date = datetime.datetime.fromisoformat(due_date).timestamp()
+
+    if time.time() < unix_due_date:
+        print("true")
+    else:
+        print("false")
+
+
+if __name__ == "__main__":
+    main()
